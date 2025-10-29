adf0fc391a48f81abdcbe4d936982e0c274762d5
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2025-10-29 10:12:51 -0500

    Delete Real-time apps.js

--- Real-time apps.js
+++
@@ -1,8 +0,0 @@
-// Client-side prediction
-let predictedPosition = currentPosition + velocity * deltaTime;
-
-// Server reconciliation
-socket.on('serverUpdate', (serverPosition) => {
-  const error = serverPosition - predictedPosition;
-  predictedPosition += error * 0.5; // Smooth correction
-});
