e4e054d3875778de9565635807f413ce30da9f2d
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2025-11-09 14:14:29 -0600

    Update C++ cents.cpp

--- C++ cents.cpp
+++ C++ cents.cpp
@@ -1,1 +1,10 @@
-#include <iostream>#include <iomanip>using namespace std;int main(){    float usdollar_amt;    cout << "NOTE: If you want to enter a dollar amount, use a whole number of 1 or greater. "         << "To enter cents, please use a decimal value." << endl;    cout << "Enter US dollar amount: ";    cin >> usdollar_amt;    if ((usdollar_amt > 0.0) && (usdollar_amt <= 0.99))    {        cout << fixed << setprecision(2) << usdollar_amt << "¢" << endl;    }    else    {        cout << fixed << setprecision(2) << "$" << usdollar_amt << endl;    }    return 0;}
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+
+{    
+float usdollar_amt;    cout << "NOTE: If you want to enter a dollar amount, use a whole number of 1 or greater. "         << "To enter cents, please use a decimal value." << endl;    cout << "Enter US dollar amount: ";    cin >> usdollar_amt;    if ((usdollar_amt > 0.0) && (usdollar_amt <= 0.99))    {        cout << fixed << setprecision(2) << usdollar_amt << "¢" << endl;    }    else    {        cout << fixed << setprecision(2) << "$" << usdollar_amt << endl;    }    
+
+return 0;}
